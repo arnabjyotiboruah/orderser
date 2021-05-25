cca58be4099dab904cc7b3b570a2f141e04db785
Arnab boruah <arnabboruah50@gmail.com>
2021-05-26 00:23:43 +0530

    first commit

---
+++ src/main/java/com/javatechie/aws/cicd/example/Order.java
@@ -0,0 +1,45 @@
+package com.javatechie.aws.cicd.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Order {
+
+    private int id;
+    private String name;
+    private int quantity;
+    private long price;
+	public Order(int i, String string, int j, int k) {
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+}
